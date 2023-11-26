8a7767e45ed5b0b1b5502afc8a7c2f54f369080f
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-11-26 12:10:18 -0500

    Update title in chats page and favicon

--- src/app/(chat)/[id]/page.tsx
+++ src/app/(chat)/[id]/page.tsx
@@ -1,9 +1,23 @@
+import { type Metadata } from 'next/types';
 import { redirect } from 'next/navigation';
 import { ChatsService } from '@/services';
 import { ChatClientLayout } from '@/components/chats/chat-client-layout';
 import { MessagesList } from '@/components/messages/messages-list';
 import { MessageInput } from '@/components/messages/message-input';
 import { ChatHeader } from '@/components/chats/chat-header';
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { id: string };
+}): Promise<Metadata> {
+	const id = params.id;
+	const chat = await ChatsService.getChatById(id);
+	return {
+		title: `${chat?.name ?? ''} | Chat Group`,
+		description: chat?.description,
+	};
+}
 
 export default async function ChatPage({
 	params: { id },
